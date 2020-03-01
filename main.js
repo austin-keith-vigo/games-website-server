b6b5e483b029e8dec3fdb4a4063a50c3581d293b
Austin Vigo <avigo@uci.edu>
2020-03-01 01:34:00 -0800

    Can communicate with react-app
    can communicate with react-app

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ var io = require('socket.io').listen(server);
 var users = [];
 var connections = [];
 
-server.listen(3000);
+server.listen(5000);
 console.log('Server running...');
+
 app.get('/', function(request, response){
+
+    // Simple html for the express app
     response.sendFile(__dirname + '/index.html');
 });
 
+// What happens when a user creates a socket to this api
 io.sockets.on('connection', function(socket) {
     connections.push(socket);
     console.log('Connections: %s sockets connected', connections.length);
+
 });
 
